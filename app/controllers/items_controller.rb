3361590f09c1ce4510ac322a94b5f34e6c5948e6
Angelina <angelinarwhite@gmail.com>
2022-07-03 23:02:13 -0400

    commit tree 3224d30bc4be3fefd9e683e1b244bfd7447c8589

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,44 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: created
+
+    # if item.valid?
+    #   render json: item, status: created
+    # else
+    #   render json: { errors: item.errors }, status:
+    #   :unprocessable_entity
+    # end
+    
+  end
+
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id, :created_at, :updated_at)
+  end
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
